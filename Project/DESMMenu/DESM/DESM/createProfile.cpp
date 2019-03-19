5a01afd67f78712736409003cc6d2081dfecdd6f
Dragomegak <stevenle6569@gmail.com>
2019-03-19 09:32:18 -0400

    Creation of file + appending working but not if file exists

--- Project/DESMMenu/DESM/DESM/createProfile.cpp
+++ Project/DESMMenu/DESM/DESM/createProfile.cpp
@@ -4,6 +4,7 @@
 #include <QMessageBox>
 #include <QDesktopServices>
 #include <QUrl>
+#include <QTextStream>
 
 CreateProfile::CreateProfile(QWidget *parent) :
     QDialog(parent),
@@ -33,6 +34,26 @@ void CreateProfile::on_addProgram_clicked()
                 "C:\\Program Files (x86)",
                 "All Files (*.*);; Exe Files (*.exe)"
                 );
-    QDesktopServices::openUrl(QUrl("file:///"+filename, QUrl::TolerantMode));
+    //Pseudo - open file, get it ready
+    QFile file("profile.txt");
+    if (!file.open(QIODevice::ReadWrite | QIODevice::Text | QIODevice::Append))
+    return;
+
+    //goes to the end, takes the string of filename (presumably path) and appends at the end of file
+    if (file.atEnd()){
+        file.write(filename.toUtf8());
+    }
+    //check if at end of file
+    else
+        {
+        QTextStream outstream(&file);
+        outstream << filename << endl;
+        file.close();
+    }
+
+
+    //handles launching of the exe
+    //QDesktopServices::openUrl(QUrl("file:///"+filename, QUrl::TolerantMode));
     //QMessageBox::information(this, tr("File Name"), filename);
+
 }
